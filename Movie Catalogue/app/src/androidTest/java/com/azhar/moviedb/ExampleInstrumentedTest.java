2ad12dc95534ce750d86dd75f344466ecb1dd79e
DELL P75F <vanixgzy@gmail.com>
2021-09-15 09:42:57 +0700

    Movie-Catalogue

--- Movie Catalogue/app/src/androidTest/java/com/azhar/moviedb/ExampleInstrumentedTest.java
+++ Movie Catalogue/app/src/androidTest/java/com/azhar/moviedb/ExampleInstrumentedTest.java
@@ -22,6 +22,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("com.azhar.moviedb", appContext.getPackageName());
+        assertEquals("com.tugaspts_33_11rpl1.moviedb", appContext.getPackageName());
     }
 }
